4526f7a8f0780133a2e9c7e0a5fcdea20b37065b
xelamanster <xelamanster@gmail.com>
2021-02-28 00:34:17 +0100

    Add handling of custom empty values

--- io/src/main/scala/com/github/xelamanster/beanconverter/io/csv/ReadCsv.scala
+++ io/src/main/scala/com/github/xelamanster/beanconverter/io/csv/ReadCsv.scala
@@ -11,14 +11,21 @@ import java.io.File
 
 object ReadCsv extends ReadFileRow[CsvSettings] {
 
+  private val defaultEmpty = ""
+
   override def apply(settings: CsvSettings): IO[BeanReaderError, List[List[String]]] = {
     val format = new DefaultCSVFormat {
       override val delimiter = settings.delimiter
     }
-    def wholeFile = CSVReader.open(new File(settings.fileName))(format).all
+    def wholeFile = CSVReader.open(new File(settings.fileName))(format).all()
 
     IO.effect(if (settings.stripHeader) wholeFile.tail else wholeFile)
+      .map(list => if (settings.empty != defaultEmpty) list.map(_.map(replaceEmpty(settings.empty))) else list)
       .mapError(FileReadError.apply)
   }
+
+  private def replaceEmpty(empty: String)(v: String) =
+    if (v == empty) defaultEmpty
+    else v
 
 }
