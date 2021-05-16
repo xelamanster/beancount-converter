055a816af06cacdfcced5a96238eb6a90772a316
xelamanster <xelamanster@gmail.com>
2021-05-16 13:53:14 +0200

    Update to scala 3 final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,9 @@ import sbt._
 
 object Dependencies {
 
-  lazy val cats = "org.typelevel" %% "cats-core" % "2.4.2"
+  lazy val cats = "org.typelevel" %% "cats-core" % "2.6.1"
 
-  lazy val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "1.3.7"
+  lazy val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "1.3.8"
   lazy val pdfobx = "org.apache.pdfbox" % "pdfbox" % "2.0.16"
   lazy val poiOoxml = "org.apache.poi" % "poi-ooxml" % "4.1.0"
 }
